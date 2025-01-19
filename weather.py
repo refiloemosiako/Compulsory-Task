5ed1693327d50b43f1c1e21837d3abefc79cfa20
Refiloe <166641860+refiloemosiako@users.noreply.github.com>
2025-01-19 13:56:37 +0200

    Delete weather.py

--- weather.py
+++
@@ -1,7 +0,0 @@
-input1 = input("how many degrees is it today?: ")
-if input1 <= 15:
-    print("There is quite a chill today!")
-elif 15 < input1 < 26:
-    print("It seems quite warm today.")
-else:
-    print("Turn the fan on, It is very hot today!")
