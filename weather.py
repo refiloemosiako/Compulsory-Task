4d20cd19d914a03f5b3bbddf00140d5ad67908d5
refiloemosiako <refiloemosiako@gmail.com>
2025-02-04 22:41:55 +0200

    Changed variable input1 to temperature

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
-input1 = input("how many degrees is it today?: ")
-if input1 <= 15:
+temperature = input("how many degrees is it today?: ")
+if temperature <= 15:
     print("There is quite a chill today!")
-elif 15 < input1 < 26:
+elif 15 < temperature < 26:
     print("It seems quite warm today.")
 else:
     print("Turn the fan on, It is very hot today!")
